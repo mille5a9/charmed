c563be1e05284ab5e128f16a6525f4c344e830e7
mille5a9 <andrew.mille5a9@outlook.com>
2018-06-18 15:22:32 -0400

    restructured filesystem, created subdirectories for each project and the Abstract Data Type assets

--- charmed.hpp
+++
@@ -1,8 +0,0 @@
-/*This header is a place for code that may be useful for multiple different
-  projects in this directory
-  Created 2 February 2018, Last edit 16 February 2018*/
-
-#ifndef CHARMED_HPP_DEFINED
-#define CHARMED_HPP_DEFINED
-
-#endif
