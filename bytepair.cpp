f0fd54fe691ea4ec36acd9889db3c51979f5ecab
mille5a9 <andrew.mille5a9@outlook.com>
2018-06-18 12:32:53 -0400

    created bytepair.cpp to start byte pair encoding implementation

---
+++ bytepair.cpp
@@ -0,0 +1,9 @@
+//Andrew Miller
+//University of Cincinnati Computer Engineering '21
+
+//Implementation of byte-pair encoding concept to
+//compress a collection of bytes
+
+int main() {
+    return 0;
+}
