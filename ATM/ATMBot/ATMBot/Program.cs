5c99a473be889a6db80f7d6bb910b7b336641fb9
Andrewmiller <andrew.miller@fortech-usa.com>
2018-11-07 09:30:24 -0500

    Fleshed out discord bot from tutorial, added ATMdb to ATMBot.sln so the bot can remember data

--- ATM/ATMBot/ATMBot/Program.cs
+++ ATM/ATMBot/ATMBot/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
 
 namespace ATMBot
 {
+    public class MyCommands
+    {
+        [Command("hi")]
+        public async Task Hi(CommandContext ctx)
+        {
+            await ctx.RespondAsync($"👋 Hi, {ctx.User.Mention}!");
+        }
+    }
+
     class Program
     {
+        static DiscordClient discord;
+        static CommandsNextModule commands;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "NTA5NzIzNzgyMTk4MzI5MzQ1.DsR9pg.KBGVl5aiAGO7iIYdZrpt51gdYws",
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+            });
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = "@ATM "
+            });
+            commands.RegisterCommands<MyCommands>();
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
         }
     }
 }
