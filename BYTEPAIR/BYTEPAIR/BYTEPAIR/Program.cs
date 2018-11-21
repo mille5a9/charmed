b379e578f681030abbff1ae867d61d06678489ec
mille5a9 <andrew.mille5a9@outlook.com>
2018-11-20 20:40:38 -0500

    BYTEPAIR actually successfully compressed the given dataset - albeit slowly

--- BYTEPAIR/BYTEPAIR/BYTEPAIR/Program.cs
+++ BYTEPAIR/BYTEPAIR/BYTEPAIR/Program.cs
@@ -7,46 +7,41 @@ namespace BYTEPAIR
 
     public class BytePair
     {
-        public BytePair(int quantity = 2000)
+        public BytePair(int quantity = 10000)
         {
             Random x = new Random();
             byte[] bytes = new byte[quantity];
             for (int i = 0; i < quantity; i++)
             {
-                bytes[i] = (byte)x.Next(256);
+                bytes[i] = (byte)x.Next(16);
             }
-            _bits = new BitArray(bytes);
+            _bytes = bytes;
         }
 
         public void Compress()
         {
-            List<BitArray> bitsets = new List<BitArray>();
-            List<BitArray> uniquebitsets = new List<BitArray>();
+            List<Tuple<byte, byte>> bitsets = new List<Tuple<byte, byte>>();
+            List<Tuple<byte, byte>> uniquebitsets = new List<Tuple<byte, byte>>();
             List<int> occurences = new List<int>();
-            bool currentbit = false;
-            int initialsize = _bits.Count;
+            int initialsize = _bytes.Length;
             int maxindex = 0;
             do
             {
                 bitsets.Clear();
                 occurences.Clear();
                 uniquebitsets.Clear();
-                for (int i = 0; i < _bits.Count - 8; i += 16)
+                for (int i = 0; i < _bytes.Length - 1; i += 2)
                 {
-                    bool[] arr = { _bits.Get(i), _bits.Get(i + 1), _bits.Get(i + 2), _bits.Get(i + 3),
-                        _bits.Get(i + 4), _bits.Get(i + 5), _bits.Get(i + 6), _bits.Get(i + 7),
-                        _bits.Get(i + 8), _bits.Get(i + 9), _bits.Get(i + 10), _bits.Get(i + 11),
-                        _bits.Get(i + 12), _bits.Get(i + 13), _bits.Get(i + 14), _bits.Get(i + 15) };
-                    BitArray add = new BitArray(arr);
-                    bitsets.Add(add);
-                    if (uniquebitsets.Contains(add) == false)
+                    Tuple<byte, byte> here = new Tuple<byte, byte>(_bytes[i], _bytes[i + 1]);
+                    bitsets.Add(here);
+                    if (uniquebitsets.Contains(here) == false)
                     {
-                        uniquebitsets.Add(add);
+                        uniquebitsets.Add(here);
                         occurences.Add(1);
                     }
                     else
                     {
-                        occurences[uniquebitsets.IndexOf(add)]++;
+                        occurences[uniquebitsets.IndexOf(here)]++;
                     }
                 }
 
@@ -57,54 +52,45 @@ namespace BYTEPAIR
                 bool stay = true;
                 for (; stay; freebyte++)
                 {
-                    for (int i = 0; i < _bits.Count - 8; i += 16)
+                    for (int i = 0; i < _bytes.Length; i++)
                     {
-                        bool[] arr = { _bits.Get(i), _bits.Get(i + 1), _bits.Get(i + 2), _bits.Get(i + 3),
-                        _bits.Get(i + 4), _bits.Get(i + 5), _bits.Get(i + 6), _bits.Get(i + 7) };
-                        if (new BitArray(freebyte) == new BitArray(arr)) break;
-                        else if (i == _bits.Count - 1) return; //if all bytes are used
+                        if (freebyte == _bytes[i]) break;
+                        else if (i == _bytes.Length - 1) return; //if all bytes are used
                         stay = false;
                     }
                 }
                 freebyte--; //now freebyte is the int value of a sequence of 8 bits that does not appear in _bits
-                byte[] replaced = new byte[2];
+                byte first = uniquebitsets[maxindex].Item1;
+                byte second = uniquebitsets[maxindex].Item2;
                 byte convertbyte = (byte)freebyte;
-                BitArray convertbit = new BitArray(convertbyte);
-                uniquebitsets[maxindex].CopyTo(replaced, 0);
-                _key.Push(new Tuple<byte, byte, byte>(convertbyte, replaced[0], replaced[1]));
+                _key.Push(new Tuple<byte, byte, byte>(convertbyte, first, second));
 
 
+                byte[] old_bytes = _bytes;
                 List<byte> new_bytes = new List<byte>();
-                for (int i = 0; i < _bits.Count - 8; i += 16)
+                for (int i = 0; i < old_bytes.Length - 1; i += 2)
                 {
-                    bool[] arr1values = new bool[8];
-                    for (int j = 0; j < 7; j++) arr1values[j] = _bits.Get(i + j);
-                    bool[] arr2values = new bool[8];
-                    for (int j = 0; j < 7; j++) arr2values[j] = _bits.Get(i + j + 8);
-                    BitArray arr1 = new BitArray(arr1values);
-                    BitArray arr2 = new BitArray(arr2values);
-                    byte[] totalarr = new byte[2];
-                    arr1.CopyTo(totalarr, 0);
-                    arr2.CopyTo(totalarr, 1);
-                    uniquebitsets[maxindex].CopyTo(replaced, 0);
-                    BitArray mayberemove = new BitArray(totalarr);
-                    if (mayberemove == uniquebitsets[maxindex])
+                    Tuple<byte, byte> here = new Tuple<byte, byte>(_bytes[i], _bytes[i + 1]);
+                    if (here.Item1 == uniquebitsets[maxindex].Item1 && here.Item2 == uniquebitsets[maxindex].Item2)
                     {
                         new_bytes.Add(convertbyte);
                     }
                     else
                     {
-                        new_bytes.Add(totalarr[0]);
-                        new_bytes.Add(totalarr[1]);
+                        new_bytes.Add(here.Item1);
+                        new_bytes.Add(here.Item2);
                     }
                 }
-                _bits = new BitArray(new_bytes.ToArray());
+                //byte[] newbyteparser = new byte[new_bytes.Count];
+                //for (int i = 0; i < new_bytes.Count; i++) { newbyteparser[i] = new_bytes[i]; }
+                _bytes = new byte[new_bytes.Count];
+                _bytes = new_bytes.ToArray();
                 Console.Write(".");
 
                 } while (occurences[maxindex] > 3);
-            float compercent = (1 - (float)initialsize / (_bits.Count) * 100);
+            float compercent = (1 - _bytes.Length / (float)initialsize) * 100;
             Console.WriteLine("\nCompression Complete!\n");
-            Console.WriteLine("The data is now of length " + _bits.Count + "!\n");
+            Console.WriteLine("The data is now of length " + _bytes.Length + "!\n");
             Console.WriteLine("I mean... it WAS a whopping " + initialsize + "...\n");
             Console.WriteLine("It was compressed by " + compercent + "%!\n");
         }
@@ -114,9 +100,9 @@ namespace BYTEPAIR
 
         }
 
-        public BitArray GetBits() { return _bits; }
+        public byte[] GetBytes() { return _bytes; }
 
-        private BitArray _bits;
+        private byte[] _bytes;
         private Stack<Tuple<byte, byte, byte>> _key = new Stack<Tuple<byte, byte, byte>>(); //holds the replacement byte and the two bytes it represents
     }
 
